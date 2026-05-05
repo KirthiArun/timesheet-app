cc120a4499b2b747b5e0659bf84a4829b37796e6
Kirthika Selvaraj <kirthika.selvaraj88@gmail.com>
2026-05-05 13:48:07 +0530

    fix: bind gunicorn to PORT env var

--- app.py
+++ app.py
@@ -1144,4 +1144,5 @@ with app.app_context():
     init_db()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
